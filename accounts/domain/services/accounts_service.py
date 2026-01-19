2dc2d7f2861f0320118ea774e4ca67143a2f3777
Singorenko <cbaezenko@gmail.com>
2026-01-19 10:42:46 +0000

    Add service logout with his test

--- accounts/domain/services/accounts_service.py
+++ accounts/domain/services/accounts_service.py
@@ -1,9 +1,13 @@
-from typing import Dict, Any
+from typing import Dict, Any, cast
+
+from rest_framework.exceptions import ValidationError
 from accounts.infrastructure.repositories.accounts_write_repository import (
     create_account_repository,
 )
 import logging
 from core.exceptions.bd import AccountAlreadyExistsError, RepositoryError
+from rest_framework_simplejwt.tokens import RefreshToken, Token
+
 
 logger = logging.getLogger(__name__)
 
@@ -17,3 +21,13 @@ def register_account(email, password) -> Dict[str, Any]:
     except RepositoryError as e:
         logger.exception("Repository error during account registration")
         raise AccountAlreadyExistsError from e
+
+
+def logout_account(refresh_token: str) -> None:
+    if not refresh_token:
+        raise ValidationError("refresh token required")
+    try:
+        token = RefreshToken(cast(Token, refresh_token))
+        token.blacklist()
+    except Exception:
+        raise ValidationError("invalid or expired refresh token")
