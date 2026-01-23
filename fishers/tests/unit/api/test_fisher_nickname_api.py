444281acf0326d1529fe294ac2306872ded24d16
Singorenko <cbaezenko@gmail.com>
2026-01-23 07:33:07 +0000

    test(fishers): add API view test for nickname update success

---
+++ fishers/tests/unit/api/test_fisher_nickname_api.py
@@ -0,0 +1,41 @@
+from pytest import fixture
+from rest_framework.test import APIClient
+from django.urls import reverse
+from unittest.mock import patch
+from rest_framework import status
+
+
+@fixture
+def api_client():
+    return APIClient()
+
+
+@fixture
+def authenticated_user(api_client, django_user_model):
+    user = django_user_model.objects.create_user(
+        username="user1@user.com", password="user1user1"
+    )
+    api_client.force_authenticate(user=user)
+    return api_client, user
+
+
+@patch("fishers.api.views.set_fisher_nickname")
+def test_fisher_nickname_success(mock_service, authenticated_user):
+    """
+    GIVEN an authenticated user
+    WHEN the nickname update endpoint is called
+    THEN it returns a confirmation message with HTTP 200
+    """
+
+    payload = {"nickname": "OldFisher"}
+    mock_service.return_value = f"Fisher nickname updated to {payload['nickname']}"
+    url = reverse("fishers:nickname")
+    client, user = authenticated_user
+    response = client.patch(url, payload, format="json")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert (
+        response.data["message"] == f"Fisher nickname updated to {payload['nickname']}"
+    )
+
+    mock_service.assert_called_once_with(user=user, nickname=payload["nickname"])
