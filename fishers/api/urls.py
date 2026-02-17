d6158db7321cc4d0cc13368fa25fe66a72f97105
Singorenko <cbaezenko@gmail.com>
2026-02-17 10:44:42 +0000

    refactor(fisher): rename details-me endpoint to details_me for consistent naming

--- fishers/api/urls.py
+++ fishers/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from fishers.api.views import FisherMeAPIView, FisherNicknameAPIView
+from fishers.api.views import (
+    FisherMeAPIView,
+    FisherNicknameAPIView,
+    FisherChangeZoneAPIView,
+)
 
 app_name = "fishers"
 
 urlpatterns = [
-    path("me/", FisherMeAPIView.as_view(), name="details-me"),
+    path("me/", FisherMeAPIView.as_view(), name="details_me"),
     path("nickname/", FisherNicknameAPIView.as_view(), name="nickname"),
+    path("change-zone/", FisherChangeZoneAPIView.as_view(), name="change_zone"),
 ]
