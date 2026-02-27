2e47e43e3767be8b313401e12c89e40e69dd5531
Singorenko <cbaezenko@gmail.com>
2026-02-27 08:42:23 +0000

    test(inventory): refactor integration tests for fish description API to use factories

--- inventory/tests/integration/api/test_inventory_fish_description_integration_api.py
+++ inventory/tests/integration/api/test_inventory_fish_description_integration_api.py
@@ -4,10 +4,8 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from typing import cast
 import pytest
-
-from fish.models import Fish
-from fishers.models import Fisher
-from inventory.models import FisherFish
+from inventory.tests.factories.fisher_fish_factory import FisherFishFactory
+from inventory.tests.factories.fisher_factory import FisherFactory
 
 
 @pytest.fixture
@@ -16,27 +14,27 @@ def api_client():
 
 
 @pytest.fixture
-def authenticated_user(django_user_model, api_client):
-    user = django_user_model.objects.create_user(
-        username="usertest@user.com", password="usertest"
-    )
-    refresh = cast(RefreshToken, RefreshToken.for_user(user=user))
+def authenticated_user_with_fisher_profile_and_fisher_fish(db, api_client):
+    fisher = FisherFactory()
+    fisherFish = FisherFishFactory(fisher=fisher)
+
+    refresh = cast(RefreshToken, RefreshToken.for_user(user=fisher.user))
     access_token = str(refresh.access_token)
 
     api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
-    return api_client, user
+    return api_client, fisher.user, fisherFish
 
 
 @pytest.fixture
-def unauthenticated_user(django_user_model):
-    return django_user_model.objects.create_user(
-        username="usertest@user.com", password="usertest"
-    )
+def unauthenticated_user_with_fisher_profile_and_fisher_fish(db):
+    fisher = FisherFactory()
+    fisherFish = FisherFishFactory(fisher=fisher)
+    return fisher.user, fisherFish
 
 
 class TestInventoryFishDescriptionIntegraionSuccess:
     def test_inventory_description_integration_api_returns_data(
-        self, authenticated_user
+        self, authenticated_user_with_fisher_profile_and_fisher_fish
     ):
         """
         GIVEN a valid request to the inventory description endpoint
@@ -44,13 +42,7 @@ class TestInventoryFishDescriptionIntegraionSuccess:
         THEN the response should contain the updated description and a success status
         """
         url = reverse("inventory:fisher_fish_description")
-        client, user = authenticated_user
-
-        fish = Fish.objects.get(fish_id=1)
-        fisher = Fisher.objects.create(user=user, nickname=user)
-        fisherFish = FisherFish.objects.create(
-            fisher=fisher, fish=fish, weight=1, length=1
-        )
+        client, _, fisherFish = authenticated_user_with_fisher_profile_and_fisher_fish
 
         payload = {"pk": fisherFish.pk, "description": "A new description"}
         response = client.patch(url, payload, format="json")
@@ -58,20 +50,22 @@ class TestInventoryFishDescriptionIntegraionSuccess:
 
 
 class TestInventoryFishDescriptionIntegraionErrors:
-    def test_inventory_description_integration_api_invalid_pk(self, authenticated_user):
+    def test_inventory_description_integration_api_invalid_pk(
+        self, authenticated_user_with_fisher_profile_and_fisher_fish
+    ):
         """
         GIVEN an invalid primary key in the request to the inventory description endpoint
         WHEN the request is made
         THEN the response should contain an error message and a 500 status code
         """
         url = reverse("inventory:fisher_fish_description")
-        client, _ = authenticated_user
+        client, _, _ = authenticated_user_with_fisher_profile_and_fisher_fish
         payload = {"pk": 455, "description": "A new description"}
         response = client.patch(url, payload, format="json")
         assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
 
     def test_inventory_description_integration_api_invalid_description(
-        self, authenticated_user
+        self, authenticated_user_with_fisher_profile_and_fisher_fish
     ):
         """
         GIVEN an invalid description in the request to the inventory description endpoint
@@ -79,43 +73,28 @@ class TestInventoryFishDescriptionIntegraionErrors:
         THEN the response should contain an error message and a 400 status code
         """
         url = reverse("inventory:fisher_fish_description")
-        client, user = authenticated_user
+        client, _, fisherFish = authenticated_user_with_fisher_profile_and_fisher_fish
 
-        fish = Fish.objects.get(fish_id=1)
-        fisher = Fisher.objects.create(user=user, nickname=user)
-        fisherFish = FisherFish.objects.create(
-            fisher=fisher, fish=fish, weight=1, length=1
-        )
+        invalid_description = "x" * 300
 
         payload = {
             "pk": fisherFish.pk,
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
-            " nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
-            "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
-            "Excepteur sint occaecat cupidatat non proident, "
-            "sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "description": invalid_description,
         }
         response = client.patch(url, payload, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_inventory_description_integration_api_unauthenticated(
-        self, unauthenticated_user, api_client
+        self, unauthenticated_user_with_fisher_profile_and_fisher_fish, api_client
     ):
-        """GIVEN an unauthenticated user making a request to the inventory description endpoint
+        """
+        GIVEN an unauthenticated user making a request to the inventory description endpoint
         WHEN the request is made
         THEN the response should contain an error message and a 401 status code
         """
         url = reverse("inventory:fisher_fish_description")
 
-        fish = Fish.objects.get(fish_id=1)
-        fisher = Fisher.objects.create(
-            user=unauthenticated_user, nickname=unauthenticated_user
-        )
-        fisherFish = FisherFish.objects.create(
-            fisher=fisher, fish=fish, weight=1, length=1
-        )
+        user, fisherFish = unauthenticated_user_with_fisher_profile_and_fisher_fish
 
         payload = {"pk": fisherFish.pk, "description": "A new description"}
         response = api_client.patch(url, payload, format="json")
