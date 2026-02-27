0fc80e1a3a75438f98661b445f22e030a67a3393
Singorenko <cbaezenko@gmail.com>
2026-02-27 08:08:47 +0000

    test(inventory): add integration tests for inventory item list repository

--- inventory/tests/integration/repositories/test_inventory_item_list_repository_integration.py
+++ inventory/tests/integration/repositories/test_inventory_item_list_repository_integration.py
@@ -1,40 +1,53 @@
 import pytest
-from fishers.models import Fisher
+from core.exceptions.domain import FisherNotFoundError
 from inventory.infrastructure.repositories.inventory_read_repository import (
     get_inventory_item_list_repository,
 )
-from inventory.models import FisherItem
-from store.models import ItemStore
+from inventory.tests.factories.fisher_factory import FisherFactory
+from inventory.tests.factories.fisher_item_factory import FisherItemFactory
+from inventory.tests.factories.user_factory import UserFactory
 
 
 @pytest.fixture
-def user_with_items(django_user_model):
-    """
-    GIVEN a user with fisher and pre-purchased items
-    """
-    user = django_user_model.objects.create_user(
-        username="user_test@user.com", password="user_test"
-    )
-    fisher = Fisher.objects.create(user=user, nickname="user_test")
-    itemStore, _ = ItemStore.objects.get_or_create(
-        code="ROD_BASIC", defaults={"name": "Basic Rod", "price": 100, "effect": 0.2}
-    )
-
-    FisherItem.objects.update_or_create(
-        fisher=fisher, item=itemStore, defaults={"quantity": 1}
-    )
-    return user
+def user_with_fisher_profile_and_fisher_item(db):
+    fisher = FisherFactory()
+    fisherItem = FisherItemFactory(fisher=fisher)
+    return fisher.user, fisherItem
 
 
-@pytest.mark.integration
-@pytest.mark.django_db
-def test_get_inventory_item_list_repository_returns_data(user_with_items):
-    """
-    GIVEN a user with a fisher and purchased items
-    WHEN the inventory repository is queried for the user's items
-    THEN it returns a list of items with correct code, name and quantity
-    """
-    result = get_inventory_item_list_repository(user=user_with_items)
-    assert result == [
-        {"item_code": "ROD_BASIC", "item_name": "Basic Rod", "quantity": 1}
-    ]
+@pytest.fixture
+def user_without_fisher_profile(db):
+    return UserFactory()
+
+
+class TestInventoryItemListRepositoryIntegrationSuccess:
+    def test_get_inventory_item_list_repository_returns_data(
+        self, user_with_fisher_profile_and_fisher_item
+    ):
+        """
+        GIVEN a user with a fisher and purchased items
+        WHEN the inventory repository is queried for the user's items
+        THEN it returns a list of items with correct code, name and quantity
+        """
+        user, fisherItem = user_with_fisher_profile_and_fisher_item
+        result = get_inventory_item_list_repository(user=user)
+        assert result == [
+            {
+                "item_code": fisherItem.item.code,
+                "item_name": fisherItem.item.name,
+                "quantity": fisherItem.quantity,
+            }
+        ]
+
+    def test_get_inventory_item_list_repository_raises_fisher_not_found(
+        self, user_without_fisher_profile
+    ):
+        """
+        GIVEN a user without a fisher profile
+        WHEN the inventory repository is queried for the user's items
+        THEN it raise a FisherNotFoundError
+        """
+        user = user_without_fisher_profile
+        with pytest.raises(FisherNotFoundError) as exc:
+            get_inventory_item_list_repository(user=user)
+        assert str(exc.value) == FisherNotFoundError.default_detail
