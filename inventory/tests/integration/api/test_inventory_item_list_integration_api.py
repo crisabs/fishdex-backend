225e31435fc5866c949aeb617876e6c906112086
Singorenko <cbaezenko@gmail.com>
2026-03-01 07:56:25 +0000

    test(inventory): refactor integration tests for inventory item list API to use factories

--- inventory/tests/integration/api/test_inventory_item_list_integration_api.py
+++ inventory/tests/integration/api/test_inventory_item_list_integration_api.py
@@ -3,12 +3,12 @@ from rest_framework.test import APIClient
 from django.contrib.auth.models import AbstractBaseUser
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
-from unittest.mock import patch
 from django.urls import reverse
 import pytest
-
-from core.exceptions.bd import RepositoryError
-from core.exceptions.domain import FisherNotFoundError
+from inventory.tests.factories.fisher_factory import FisherFactory
+from inventory.tests.factories.fisher_item_factory import FisherItemFactory
+from inventory.models import FisherItem
+from inventory.tests.factories.user_factory import UserFactory
 
 
 @pytest.fixture
@@ -17,12 +17,25 @@ def api_client():
 
 
 @pytest.fixture
-def authenticated_user(
-    api_client, django_user_model
+def authenticated_user_with_fisher_and_fisher_item(
+    db, api_client
+) -> Tuple[APIClient, AbstractBaseUser, FisherItem]:
+    fisher = FisherFactory()
+    fisherItem = FisherItemFactory(fisher=fisher)
+
+    refresh = cast(RefreshToken, RefreshToken.for_user(user=fisher.user))
+    access_token = str(refresh.access_token)
+    api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+
+    return (api_client, fisher.user, fisherItem)
+
+
+@pytest.fixture
+def authenticated_user_without_fisher(
+    db, api_client
 ) -> Tuple[APIClient, AbstractBaseUser]:
-    user = django_user_model.objects.create_user(
-        username="user_test@user.com", password="user_test"
-    )
+    user = UserFactory()
+
     refresh = cast(RefreshToken, RefreshToken.for_user(user=user))
     access_token = str(refresh.access_token)
     api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
@@ -30,26 +43,34 @@ def authenticated_user(
     return (api_client, user)
 
 
-@patch("inventory.api.views.get_inventory_item_list")
-def test_intentory_item_list_api_returns_data(mock_service, authenticated_user):
+@pytest.fixture
+def unauthenticated_user(db):
+    return UserFactory()
+
+
+def test_intentory_item_list_api_returns_data(
+    authenticated_user_with_fisher_and_fisher_item,
+):
     """
     GIVEN an authenticated user and the inventory service returns an item list
     WHEN the inventory item list endpoint is requested
     THEN the API responds with HTTP 200 and the inventory items in the response body
     """
 
-    mock_service.return_value = [
-        {"item_code": "ROD_BASIC", "item_name": "Basic Rod", "quantity": 13},
+    client, _, fisherItem = authenticated_user_with_fisher_and_fisher_item
+    expected_return_value = [
+        {
+            "item_code": fisherItem.item.code,
+            "item_name": fisherItem.item.name,
+            "quantity": fisherItem.quantity,
+        },
     ]
     url = reverse("inventory:item-list")
-    client, user = authenticated_user
     response = client.get(url)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.data["success"] is True
-    assert response.data["result"] == mock_service.return_value
-
-    mock_service.assert_called_once_with(user=user)
+    assert response.data["result"] == expected_return_value
 
 
 def test_inventory_item_list_api_unauthenticated_user(api_client):
@@ -64,9 +85,8 @@ def test_inventory_item_list_api_unauthenticated_user(api_client):
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-@patch("inventory.api.views.get_inventory_item_list")
-def test_inventory_item_list_api_returns_500_when_service_raises_fisher_not_found_error(
-    mock_service, authenticated_user
+def test_inventory_item_list_api_returns_500_when_service_raises_fisher_not_found(
+    authenticated_user_without_fisher,
 ):
     """
     GIVEN an authenticated user and the inventory service raises a FisherNotFoundError
@@ -74,33 +94,8 @@ def test_inventory_item_list_api_returns_500_when_service_raises_fisher_not_foun
     THEN the API responds with HTTP 500 internal server error
     """
 
-    mock_service.side_effect = FisherNotFoundError()
-
     url = reverse("inventory:item-list")
-    client, user = authenticated_user
+    client, _ = authenticated_user_without_fisher
     response = client.get(url)
 
     assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-
-    mock_service.assert_called_once_with(user=user)
-
-
-@patch("inventory.api.views.get_inventory_item_list")
-def test_inventory_item_list_api_returns_500_when_service_raises_repository_error(
-    mock_service, authenticated_user
-):
-    """
-    GIVEN an authenticated user and the inventory service raises a RepositoryError
-    WHEN the inventory item list endpoint is requested
-    THEN the API responds with HTTP 500 internal server error
-    """
-
-    mock_service.side_effect = RepositoryError()
-
-    url = reverse("inventory:item-list")
-    client, user = authenticated_user
-    response = client.get(url)
-
-    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-
-    mock_service.assert_called_once_with(user=user)
