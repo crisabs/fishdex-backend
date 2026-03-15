b01b9500975564ce0adf38d914d1542f00a07ec7
Singorenko <cbaezenko@gmail.com>
2026-03-15 09:08:08 +0000

    test(fishers): add unit tests for API fisher nickname

--- fishers/tests/unit/api/test_fisher_nickname_api.py
+++ fishers/tests/unit/api/test_fisher_nickname_api.py
@@ -1,91 +1,121 @@
 from pytest import fixture
-from rest_framework.test import APIClient
-from django.urls import reverse
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from rest_framework import status
 from core.exceptions.bd import RepositoryError
+from rest_framework.test import APIRequestFactory
+from fishers.api.views import FisherNicknameAPIView
+
+API_PATH = "/fishers/nickname/"
 
 
 @fixture
-def api_client():
-    return APIClient()
+def api_factory():
+    return APIRequestFactory()
 
 
 @fixture
-def authenticated_user(api_client, django_user_model):
-    user = django_user_model.objects.create_user(
-        username="user1@user.com", password="user1user1"
-    )
-    api_client.force_authenticate(user=user)
-    return api_client, user
+def api_view():
+    return FisherNicknameAPIView.as_view()
 
 
-@patch("fishers.api.views.set_fisher_nickname")
-def test_fisher_nickname_success(mock_service, authenticated_user):
-    """
-    GIVEN an authenticated user
-    WHEN the nickname update endpoint is called
-    THEN it returns a confirmation message with HTTP 200
-    """
-
-    payload = {"nickname": "OldFisher"}
-    mock_service.return_value = f"Fisher nickname updated to {payload['nickname']}"
-    url = reverse("fishers:nickname")
-    client, user = authenticated_user
-
-    response = client.patch(url, payload, format="json")
-
-    assert response.status_code == status.HTTP_200_OK
-    assert (
-        response.data["message"] == f"Fisher nickname updated to {payload['nickname']}"
-    )
-
-    mock_service.assert_called_once_with(user=user, nickname=payload["nickname"])
+@fixture
+def authenticated_user():
+    fake_user = MagicMock()
+    fake_user.is_authenticated = True
+    return fake_user
 
 
-def test_fisher_nickname_invalid_payload(authenticated_user):
-    """
-    GIVEN an authenticated user
-    WHEN payload is invalid
-    THEN the API returns HTTP 400
-    """
-    client, user = authenticated_user
-    payload = {"nickname": ""}
-    url = reverse("fishers:nickname")
-
-    response = client.patch(url, payload, format="json")
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+@fixture
+def unauthenticated_user():
+    fake_user = MagicMock()
+    fake_user.is_authenticated = False
+    return fake_user
 
 
-@patch("fishers.api.views.set_fisher_nickname")
-def test_fisher_nickname_internal_server_error(mock_service, authenticated_user):
-    """
-    GIVEN an authenticated user
-    WHEN the nickname update service raises a repository error
-    THEN the API returns HTTP 500
-    """
+class TestFisherNicknameSuccess:
 
-    client, user = authenticated_user
-    payload = {"nickname": "nickname"}
-    url = reverse("fishers:nickname")
+    @patch("fishers.api.views.FisherNicknameAPIView.permission_classes", new=[])
+    @patch("fishers.api.views.set_fisher_nickname")
+    def test_fisher_nickname_success(
+        self, mock_service, authenticated_user, api_factory, api_view
+    ):
+        """
+        GIVEN an authenticated user
+        WHEN the nickname update endpoint is called
+        THEN it returns a confirmation message with HTTP 200
+        """
 
-    mock_service.side_effect = RepositoryError
+        payload = {"nickname": "OldFisher"}
+        mock_service.return_value = f"Fisher nickname updated to {payload['nickname']}"
 
-    response = client.patch(url, payload, format="json")
+        request = api_factory.patch(path=API_PATH, data=payload, format="json")
+        request.user = authenticated_user
 
-    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        response = api_view(request)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert (
+            response.data["message"]
+            == f"Fisher nickname updated to {payload['nickname']}"
+        )
+
+        mock_service.assert_called_once_with(
+            user=request.user, nickname=payload["nickname"]
+        )
 
 
-def test_fisher_nickname_unauthenticated_user(api_client):
-    """
-    GIVEN an user without authentication
-    WHEN calling the nickname update endpoint
-    THEN returns HTTP 401 Unauthorized
-    """
-    payload = {"nickname": "nickname"}
-    url = reverse("fishers:nickname")
+class TestFisherNicknameErrors:
 
-    response = api_client.patch(url, payload, format="json")
+    @patch("fishers.api.views.FisherNicknameAPIView.permission_classes", new=[])
+    def test_fisher_nickname_invalid_payload(
+        self, authenticated_user, api_factory, api_view
+    ):
+        """
+        GIVEN an authenticated user
+        WHEN payload is invalid
+        THEN the API returns HTTP 400
+        """
+        payload = {"nickname": ""}
+        request = api_factory.patch(path=API_PATH, data=payload, format="json")
+        request.user = authenticated_user
 
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        response = api_view(request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    @patch("fishers.api.views.FisherNicknameAPIView.permission_classes", new=[])
+    @patch("fishers.api.views.set_fisher_nickname")
+    def test_fisher_nickname_internal_server_error(
+        self, mock_service, api_factory, api_view, authenticated_user
+    ):
+        """
+        GIVEN an authenticated user
+        WHEN the nickname update service raises a repository error
+        THEN the API returns HTTP 500
+        """
+
+        payload = {"nickname": "nickname"}
+
+        request = api_factory.patch(path=API_PATH, data=payload, format="json")
+        request.user = authenticated_user
+
+        mock_service.side_effect = RepositoryError
+
+        response = api_view(request)
+
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    def test_fisher_nickname_unauthenticated_user(
+        self, unauthenticated_user, api_factory, api_view
+    ):
+        """
+        GIVEN an user without authentication
+        WHEN calling the nickname update endpoint
+        THEN returns HTTP 401 Unauthorized
+        """
+        payload = {"nickname": "nickname"}
+
+        request = api_factory.patch(path=API_PATH, data=payload, format="json")
+        request.user = unauthenticated_user
+        response = api_view(request)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
